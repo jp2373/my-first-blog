b16e2d2ed0903946172a974493e3dcd93e1489f3
jp2373 <jeelpujara2373@gmail.com>
2020-08-24 12:15:02 +0530

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't1(ktn)pw@e43oww6o^!71y1^ea^r$nw+63!$ngk8jppzo1gpa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
